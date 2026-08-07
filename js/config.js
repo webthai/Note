5d34490724afb1de1ca03e165c7549512571c892
webthai <webmeenkuna@gmail.com>
2026-08-07 16:13:39 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // ⚠️ แก้ค่านี้เป็น URL ของ Google Apps Script Web App ที่ deploy แล้ว
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycb.../exec
-const APPS_SCRIPT_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzO5LFCa3n3eH8A8K2WGf7XeEth3YYR9rDlUm1co_xJjb9QKD8Je0hEXoaFrFYub0ukYg/execE';
